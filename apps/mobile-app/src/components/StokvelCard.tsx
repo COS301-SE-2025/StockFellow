192444489d71d848fad21a7a1c6062479cadcd3b
ltshikila <ltshikila17@gmail.com>
2025-06-18 23:52:43 +0200

    stokvelpage adjustments

--- apps/mobile-app/src/components/StokvelCard.tsx
+++ apps/mobile-app/src/components/StokvelCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react'; 
+import React from 'react';
 import { View, Text, Image, TouchableOpacity } from 'react-native';
 import { icons } from '../constants';
 import { useTheme } from '../../app/_layout';
@@ -11,43 +11,41 @@ interface StokvelCardProps {
   onPress: () => void;
 }
 
-const StokvelCard: React.FC<StokvelCardProps> = ({ 
-  name, 
-  memberCount, 
-  balance, 
-  profileImage, 
-  onPress 
+const StokvelCard: React.FC<StokvelCardProps> = ({
+  name,
+  memberCount,
+  balance,
+  profileImage,
+  onPress
 }) => {
   const { isDarkMode, colors } = useTheme();
-  
+
   return (
-    <TouchableOpacity 
+    <TouchableOpacity
       onPress={onPress}
-      style={{ 
-        height: 85, 
-      }}
-      className="flex-row items-center mb-4 p-2 rounded-lg"
+      
+      className="w-full flex-row items-center mb-1 px-2 "
     >
       {/* Left section: Icon and info */}
       <View className="flex-row items-center flex-1">
         {/* Profile Image with fallback */}
-        <View className="w-16 h-16 rounded-full bg-gray-200 items-center justify-center mr-3">
-          <Image 
-            source={profileImage ? { uri: profileImage } : icons.group}
-            className="w-14 h-14 rounded-full"
-            resizeMode={profileImage ? "cover" : "contain"}
-          />
-        </View>
+
+        <Image
+          source={profileImage ? { uri: profileImage } : icons.group}
+          className="w-20 h-20  shadow-xs shadow-[#1DA1FA]/90 self-center"
+          resizeMode={"contain"}
+        />
+
 
         <View className='flex-1 justify-center'>
-          <Text 
+          <Text
             style={{ color: isDarkMode ? '#CCCCCC' : '#1A1A1A' }}
             className="text-base font-['PlusJakartaSans-SemiBold']"
             numberOfLines={1}
           >
             {name}
           </Text>
-          <Text 
+          <Text
             style={{ color: isDarkMode ? '#AAAAAA' : '#6F6F6F' }}
             className="text-sm font-['PlusJakartaSans-Regular']"
           >
