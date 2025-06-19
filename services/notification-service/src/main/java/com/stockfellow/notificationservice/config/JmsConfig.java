f075ee373d84748219034686e9e94919187996c5
tinothecreated <tinotchirozvi@gmail.com>
2025-06-20 00:33:57 +0200

    services notifications, not complete :(

--- services/notification-service/src/main/java/com/stockfellow/notificationservice/config/JmsConfig.java
+++ services/notification-service/src/main/java/com/stockfellow/notificationservice/config/JmsConfig.java
@@ -1,7 +1,6 @@
 package com.stockfellow.notificationservice.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.activemq.ActiveMQConnectionFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,24 +16,6 @@ import jakarta.jms.ConnectionFactory;
 @Configuration
 @EnableJms
 public class JmsConfig {
-
-    @Value("${spring.activemq.broker-url}")
-    private String brokerUrl;
-
-    @Value("${spring.activemq.user}")
-    private String username;
-
-    @Value("${spring.activemq.password}")
-    private String password;
-
-    @Bean
-    public ConnectionFactory connectionFactory() {
-        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
-        connectionFactory.setBrokerURL(brokerUrl);
-        connectionFactory.setUserName(username);
-        connectionFactory.setPassword(password);
-        return connectionFactory;
-    }
 
     @Bean
     public JmsTemplate jmsTemplate(ConnectionFactory connectionFactory, MessageConverter messageConverter) {
