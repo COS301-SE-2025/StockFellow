4224650b0e7793c2e19aadb11472e5ebabe6e391
NaazneenKhan <naazneen.k@icloud.com>
2025-06-19 19:17:22 +0200

    Notification System
    Types of notifications

---
+++ services/notification-service/src/main/java/com/stockfellow/notificationservice/model/notificationType.java
@@ -0,0 +1,9 @@
+package com.stockfellow.notificationservice.model;
+
+public enum NotificationType {
+    SYSTEM_UPDATE, // For system-related updates
+    SUCCESS, // For successful operations
+    WARNING, // For warning messages
+    ERROR, // For error messages
+    INFO // For general information
+}
