9ea248132c283c1f430f2eafdc44f6ae16113abc
NaazneenKhan <naazneen.k@icloud.com>
2025-09-12 17:08:15 +0200

    config files

---
+++ services/webauthn-service/src/main/java/com/stockfellow/webauthn/WebAuthnServiceApplication.java
@@ -0,0 +1,12 @@
+package com.stockfellow.webauthn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAuthnServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebAuthnServiceApplication.class, args);
+    }
+}
