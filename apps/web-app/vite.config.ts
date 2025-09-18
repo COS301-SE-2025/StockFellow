f691ee237c0d7488e903f497b120023dce0c2429
ltshikila <ltshikila17@gmail.com>
2025-09-18 18:36:59 +0200

    TypeScript/npm workspace fix

--- apps/web-app/vite.config.ts
+++ apps/web-app/vite.config.ts
@@ -1,10 +1,15 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   server: {
     hmr: {
       overlay: false // This disables the error overlay if needed
